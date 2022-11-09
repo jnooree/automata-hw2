236bc026a5b5df92572701ca20323e8e970b089c
jnooree <jnooree@snu.ac.kr>
2022-11-09 14:22:13 +0900

    Update release script

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -eu
 
-prefix='Automata_HW1_2022-28022'
+prefix='Automata_HW2_2022-28022'
 
 rm -rf test/generated
-python3 -m pip install -r test-requirements.txt
-scripts/generate_test.py "test/test.in"
+scripts/generate_test.py "test/generated"
 
 mv test/generated .
 rm -rf test
 mv generated test
 
 cp scripts/compile.sh "${prefix}_compile.sh"
-for i in 1 2; do
-	file="${prefix}_Q${i}.sh"
-	echo "#!/bin/sh
-bin/Q${i}" >>"$file"
-	chmod +x "$file"
-done
+
+run_sh="${prefix}_run.sh"
+echo "#!/bin/sh
+bin/run" >>"$run_sh"
+chmod +x "$run_sh"
 
 zip -r "${prefix}.zip" ./*.{sh,pdf} CMakeLists.txt src test
